4fe2fddb4826e9289af3a877b42efdd44f15bcac
Reymel Sardenia <sardeniareymel@gmail.com>
2025-12-05 03:10:11 +0800

    add statement, conditional statement

--- Syntax-Analyzer/ParserG2PPL.c
+++ Syntax-Analyzer/ParserG2PPL.c
@@ -18,7 +18,9 @@ void parseError(const char *expected);
 void skip_noise_tokens();
 void parse_AssignmentStatement();
 void parse_Program();
+void parse_StatementList();
 void parse_Statement();
+void parse_StatementBlock();
 void parse_IDList();
 void parse_AttributeAccess();
 void parse_Literal();
@@ -57,6 +59,25 @@ void parse_ContentItem();
 void parse_Concat();
 void parse_ConcatElement();
 
+
+void parse_InputStatement();
+void parse_TargetID();
+void parse_PromptContent();
+void parse_ChoiceBlock();
+void parse_ChoiceList();
+
+void parse_OutputStatement();
+void parse_Content();
+void parse_OutputBody();
+void parse_OutputBlock();
+void parse_ContentItem();
+void parse_Concat();
+void parse_ConcatElement();
+
+void parse_ConditionStatement();
+void parse_ConditionalTail();
+void parse_ElifClause();
+void parse_ElseClause();
 
 int main() {
     char filetoken_name[256];
@@ -171,12 +192,22 @@ void parse_Program() {
     if(!match("START")){
         parseError("START keyword");
     }
-    parse_Statement();
+    parse_StatementList();
     if(!match("END")){
         parseError("END keyword");
     }
     printf("<program> (done)\n");
  
+}
+
+void parse_StatementList() {
+    printf("Enter <statement_list>...\n");
+    skip_noise_tokens();
+    while (!isAtEnd() && !check("END") && !check("DEDENT")) {
+        parse_Statement();
+        skip_noise_tokens();
+    }
+    printf("<statement_list> (done)\n");
 }
 void parse_Statement() {
     skip_noise_tokens();
@@ -193,6 +224,9 @@ void parse_Statement() {
         else if (check("NARRATE") || check("DIALOGUE") || check("SHOW")){
             parse_OutputStatement();
         }
+        else if (check("IF")) {
+            parse_ConditionStatement();
+        }
         else if (check("NEWLINE") || check("COMMENT") || check("COMMENT_MULTI")) {
             skip_noise_tokens();
             continue;
@@ -205,8 +239,24 @@ void parse_Statement() {
     while (check("DEDENT") || check("NEWLINE")) {
         advance();
     }
+    printf("<statement> (done) \n");
 }
 
+
+void parse_StatementBlock()
+{
+    skip_noise_tokens();
+    printf("Enter <statement_block>\n");
+    if (!match("INDENT"))
+        parseError("an INDENT");
+
+    parse_StatementList();
+
+    if (!match("DEDENT")) 
+        parseError("a DEDENT");
+
+    printf("<statement_block> (done)\n");
+}
 void parse_IDList()
 {
     printf("Enter <id_list>\n");
@@ -706,4 +756,298 @@ void parse_AttributeList()
     }
 
     printf("<attribute_list> (done)\n");
+}
+
+/* ---- OUTPUT STATEMENT ---- */
+void parse_OutputStatement() {
+    skip_noise_tokens();
+     printf("\nEnter <output_stmt>...\n");
+ 
+    // Parse output keyword
+    if(check("NARRATE") || check("DIALOGUE") || check("SHOW")) {
+        if(match("NARRATE")) printf(" -> Consumed NARRATE keyword\n");
+        else if(match("DIALOGUE")) printf(" -> Consumed DIALOGUE keyword\n");
+        else if(match("SHOW")) printf(" -> Consumed SHOW keyword\n");
+    } else {
+        parseError("a NARRATE, DIALOGUE, or SHOW");
+    }
+ 
+    if(check("IDENTIFIER")) {  //optional attribute access
+        parse_TargetID();
+    }
+ 
+    if(match("COLON")) {
+        printf(" -> Consumed COLON\n");
+    } else {
+        parseError("a COLON"); //expects a colon
+    }
+ 
+    parse_OutputBody();
+ 
+    printf("<outuput_stmt> (done) \n");
+}
+ 
+void parse_OutputBody(){
+ 
+    printf("\nEnter <output_body>...\n");
+   if(check("INDENT")){
+        parse_OutputBlock();
+   }
+   else if (check("STRING") || check("IDENTIFIER")) {
+        parse_ContentItem();
+   }
+   else {
+        parseError("an INDENT, STRING, or IDENTIFIER"); //expect an indent, string, or identifier
+   }
+ 
+    printf("<output_body> (done) \n");
+}
+ 
+void parse_OutputBlock(){
+    printf("\nEnter <output_block>...\n");
+    if(!match("INDENT")){
+        parseError("an INDENT"); //expect indent
+    }
+    printf(" -> Consumed INDENT\n");
+ 
+    parse_ContentItem();
+ 
+    if(!match("DEDENT")){
+        parseError("a DEDENT"); // expect dedent
+    }
+    printf(" -> Consumed DEDENT\n");
+ 
+    printf("<output_block> (done) \n");
+   
+}
+ 
+ 
+void parse_ContentItem(){
+    printf("\nEnter <content_item>...\n");
+ 
+    parse_Concat();
+ 
+    printf("<content_item> (done) \n");
+}
+ 
+ 
+void parse_Concat(){
+    printf("\nEnter <concat>...\n");
+ 
+    parse_ConcatElement();
+    while(!isAtEnd() && check("PLUS")){
+        match("PLUS");
+        parse_ConcatElement();
+    }
+ 
+    printf("<concat> (done) \n");
+}
+ 
+void parse_ConcatElement(){
+    printf("\nEnter <concat_element>...\n");
+ 
+    if(check("IDENTIFIER")){
+        parse_AttributeAccess();
+    } else {
+        parse_Literal();
+    }
+ 
+    printf("<concant_element> (done) \n");
+}
+ 
+
+/* ---- INPUT STATEMENT ---- */
+void parse_InputStatement(){
+ 
+    skip_noise_tokens();
+    printf("\nEnter <input_stmt>...\n");
+ 
+ 
+    if(check("ASK")){
+        match("ASK");
+        printf(" -> Consumed ASK keyword.\n");
+ 
+        if(check("IDENTIFIER")){
+            parse_TargetID();
+        }
+ 
+        parse_PromptContent();
+ 
+        if(!match("AS")){
+            parseError(" AS Keyword");  //expect an "as" keyword
+        }
+        printf(" -> Consumed AS keyword.\n");
+ 
+        if(!match("IDENTIFIER")){
+            parseError("an IDENTIFIER"); //expect an "identifier"
+        }
+        printf(" -> Consumed IDENTIFIER.\n");
+ 
+    } else if (check("CHOICE")){
+        match("CHOICE");
+        printf(" -> Consumed CHOICE keyword.\n");
+ 
+        if(!match("AS")){
+            parseError("AS Keyword"); //expect an "as" keyword
+        }
+        printf(" -> Consumed AS keyword.\n");
+ 
+        if(!match("IDENTIFIER")){
+            parseError("an IDENTIFIER"); //expect an "identifier"
+        }
+        printf(" -> Consumed IDENTIFIER.\n");
+ 
+        if(!match("COLON")){
+            parseError("a COLON"); //expect a "colon"
+        }
+        printf(" -> Consumed COLON.\n");
+ 
+        if(!match("NEWLINE")){
+            parseError("a NEWLINE"); //expect a "newLine"
+        }
+ 
+        parse_ChoiceBlock();
+ 
+    } else {
+        parseError("a CHOICE Keyword"); //expects an "CHOICe" keyword
+    }
+ 
+    printf("<input_stmt> (done) \n");
+}
+ 
+void parse_TargetID(){
+    printf("\nEnter <target_id>...\n");
+    parse_AttributeAccess();
+ 
+    printf("<target_id> (done) \n");
+}
+ 
+void parse_PromptContent(){
+    printf("\nEnter <prompt_content>...\n");
+    if(!match("STRING")){
+        parseError("a STRING_LITERAL");
+    }
+    printf(" -> Consumed STRING.\n");
+    printf("<prompt_content> (done) \n");
+}
+
+void parse_ChoiceBlock(){
+    skip_noise_tokens();
+ 
+    printf("\nEnter <choice_block>...\n");
+    if(check("INDENT")){
+        match("INDENT");
+        printf(" -> Consumed INDENT.\n");
+       
+        parse_ChoiceList();
+ 
+        if(!match("DEDENT"))
+            printf(" -> Consumed DEDENT.\n");
+        else parseError("a DEDENT"); //expects an DEDENT      
+    } else {
+        parseError("an INDENT"); //expects an "INDENT"
+    }
+   
+    printf("<choice_block> (done) \n");
+}
+ 
+//<choice_list> ::=  { “[“ <string_literal> ":" <literal> “]” “,” "NEWLINE"}
+void parse_ChoiceList() {
+    printf("\nEnter <choice_list>...\n");
+ 
+    if(!match("LBRACKET")){
+        parseError("expects a RBRACKET [") ;
+    }
+    printf(" -> Consumed '['\n");
+ 
+    if(!match("STRING")){
+        parseError("expects a STRING");
+    }
+    printf(" -> Consumed STRING_LITERAL: %s\n", currentToken()->lexeme);
+ 
+    if(!match("COLON")) {
+        parseError("a COLON");
+    }
+    printf(" -> Consumed COLON\n");
+ 
+    parse_Literal();
+ 
+    if(!match("RBRACKET")){
+        parseError("a BRACKET ]");
+    }
+    printf(" -> Consumed RBRACKET ]\n");
+ 
+    if(check("COMMA")){
+        if(match("COMMA")){
+            if(!match("NEWLINE")) parseError("expect a NEWLINE");
+            printf(" -> Consumed NEWLINE\n");
+            parse_ChoiceList();
+        }
+    }
+ 
+    printf("<choice_list> (done) \n");
+}
+/*
+<condition_stmt>       ::= "if" <expression> <conditional_tail>
+<conditional_tail>      ::= ":" <statement_block> { <elif_clause> } [ <else_clause> ]
+                | "then" <statement>
+
+<elif_clause>               ::= "elif" <expression> ":" <statement_block> 
+
+<else_clause>             ::= "else" ":" <statement_block>
+*/
+
+void parse_ConditionStatement(){
+    printf("\nEnter <condition_stmt>...\n");
+ 
+    if(!match("IF")){
+        parseError("an IF keyword"); 
+    } 
+    parse_Expression();
+    parse_ConditionalTail();
+ 
+    printf("<condition_stmt> (done) \n");
+}
+
+void parse_ConditionalTail(){
+    printf("\nEnter <conditional_tail>...\n");
+    if(!match("COLON"))
+        parseError("a COLON"); 
+
+    parse_StatementBlock();
+    while(check("ELIF")){
+        parse_ElifClause();
+    }
+    if(check("ELSE")){
+        match("ELSE");
+        if(!match("COLON"))
+            parseError("a COLON"); 
+        parse_StatementBlock();
+    }
+ 
+    printf("<conditional_tail> (done) \n");
+}
+
+void parse_ElifClause(){
+    printf("\nEnter <elif_clause>...\n");
+    if(!match("ELIF")){
+        parseError("an ELIF keyword"); 
+    } 
+    parse_Expression();
+    if(!match("COLON"))
+        parseError("a COLON"); 
+
+    parse_StatementBlock();
+    printf("<elif_clause> (done) \n");
+}
+void parse_ElseClause(){
+    printf("\nEnter <else_clause>...\n");
+    if(!match("ELSE")){
+        parseError("an ELSE keyword"); 
+    } 
+    if(!match("COLON"))
+        parseError("a COLON"); 
+
+    parse_StatementBlock();
+    printf("<else_clause> (done) \n");
 }
